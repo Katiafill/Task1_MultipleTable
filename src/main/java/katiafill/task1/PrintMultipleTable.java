ad17a29ffb85f4d87b856a148a5942c783ab0ebb
katiafill <katiafill@mail.ru>
2022-10-28 13:43:10 +0700

    Ready PrintMultipleTable

--- src/main/java/katiafill/task1/PrintMultipleTable.java
+++ src/main/java/katiafill/task1/PrintMultipleTable.java
@@ -2,30 +2,111 @@ package katiafill.task1;
 
 public class PrintMultipleTable {
 
-    private int size;
+    private final int size;
+    private final int maxCellSize;
+    private final int minCellSize;
 
     public PrintMultipleTable(int size) {
         this.size = size;
+        maxCellSize = getMaxCellSize();
+        minCellSize = getMinCellSize();
     }
 
     @Override
     public String toString() {
+        StringBuilder builder = new StringBuilder();
 
-        return "";
+        for (int row = 0; row <= size; row++) {
+            builder.append(stringRow(row));
+            builder.append("\n");
+            builder.append(horizontalBorder());
+            builder.append("\n");
+        }
+
+        return builder.toString();
     }
 
-    protected  int getMinCellSize() {
+    protected String verticalBorder() {
+        return " | ";
+    }
+
+    protected String corner() {
+        return " + ";
+    }
+
+    protected String stringRow(int row) {
+        StringBuilder stringRow = new StringBuilder();
+
+        stringRow.append(stringCellForValue(row, minCellSize));
+        for (int col = 1; col <= size; col++) {
+            stringRow.append(verticalBorder());
+            int nRow = row == 0 ? 1 : row;
+            stringRow.append(stringCellForValue(col * nRow, maxCellSize));
+        }
+
+        return stringRow.toString();
+    }
+
+    protected String horizontalBorder() {
+        StringBuilder border = new StringBuilder(maxCellSize * 2 * size);
+
+        border.append(stringBorderForSize(minCellSize));
+
+        String corner = corner();
+        String cellBorder = stringBorderForSize(maxCellSize);
+        for (int i = 0; i < size; i++) {
+            border.append(corner);
+            border.append(cellBorder);
+        }
+
+        return border.toString();
+    }
+
+    protected String stringBorderForSize(int size) {
+        StringBuilder border = new StringBuilder(size * 2);
+
+        border.append("-");
+        size--;
+        while (size > 0) {
+            border.append(" ");
+            border.append("-");
+            size--;
+        }
+
+        return border.toString();
+    }
+
+    protected String stringCellForValue(int value, int maxSize) {
+        StringBuilder stringCell = new StringBuilder(maxSize * 2);
+
+        for (int i = maxSize * 2 - 1; i > 0; i--) {
+            if (value > 0) {
+                int digit = value % 10;
+                value /= 10;
+                stringCell.append(digit);
+                i--;
+            }
+            if (i > 0) {
+                stringCell.append(" ");
+            }
+        }
+
+        stringCell.reverse();
+        return stringCell.toString();
+    }
+
+    protected int getMinCellSize() {
         int minValue = size;
-        return  numberOfDigitsFor(minValue);
+        return  numberOfDigitsForValue(minValue);
     }
 
     protected int getMaxCellSize() {
         int maxValue = getMaxValue();
-        return numberOfDigitsFor(maxValue);
+        return numberOfDigitsForValue(maxValue);
     }
 
     // TODO: Вынести в отдельный класс
-    protected int numberOfDigitsFor(int value) {
+    protected int numberOfDigitsForValue(int value) {
         int size = 1;
         while (value >= 10) {
             value /= 10;
--- src/test/java/katiafill/task1/PrintMultipleTableTest.java
+++ src/test/java/katiafill/task1/PrintMultipleTableTest.java
@@ -16,9 +16,9 @@ class PrintMultipleTableTest {
     @Test
     void testNumberOfDigits() {
         PrintMultipleTable table = new PrintMultipleTable(5);
-        int digits = table.numberOfDigitsFor(6);
+        int digits = table.numberOfDigitsForValue(6);
         assertEquals(digits, 1);
-        digits = table.numberOfDigitsFor(32*32);
+        digits = table.numberOfDigitsForValue(32*32);
         assertEquals(digits, 4);
     }
 
@@ -27,5 +27,67 @@ class PrintMultipleTableTest {
         PrintMultipleTable table = new PrintMultipleTable(5);
         int cellSize = table.getMaxCellSize();
         assertEquals(cellSize, 2);
+    }
+
+    @Test
+    void testCellStrings() {
+        PrintMultipleTable table = new PrintMultipleTable(5);
+        String cell = table.stringCellForValue(1, 2);
+        assertEquals(cell, "  1");
+
+        cell = table.stringCellForValue(25, 2);
+        assertEquals(cell, "2 5");
+
+        cell = table.stringCellForValue(1,1);
+        assertEquals(cell, "1");
+
+        cell = table.stringCellForValue(100, 3);
+        assertEquals(cell, "1 0 0");
+    }
+
+    @Test
+    void testCellBorder() {
+        PrintMultipleTable table = new PrintMultipleTable(5);
+        String border = table.stringBorderForSize(2);
+        assertEquals(border, "- -");
+    }
+
+    @Test
+    void testHorizontalBorder() {
+        PrintMultipleTable table = new PrintMultipleTable(5);
+        String border = table.horizontalBorder();
+        assertEquals(border, "- + - - + - - + - - + - - + - -");
+    }
+
+    @Test
+    void testPrint2x2() {
+        PrintMultipleTable table = new PrintMultipleTable(2);
+        String result = table.toString();
+        assertEquals(result,
+                "  | 1 | 2\n" +
+                        "- + - + -\n" +
+                        "1 | 1 | 2\n" +
+                        "- + - + -\n" +
+                        "2 | 2 | 4\n" +
+                        "- + - + -\n");
+    }
+
+    @Test
+    void testPrint5x5() {
+        PrintMultipleTable table = new PrintMultipleTable(5);
+        String result = table.toString();
+        assertEquals(result,
+                "  |   1 |   2 |   3 |   4 |   5\n" +
+                        "- + - - + - - + - - + - - + - -\n" +
+                        "1 |   1 |   2 |   3 |   4 |   5\n" +
+                        "- + - - + - - + - - + - - + - -\n" +
+                        "2 |   2 |   4 |   6 |   8 | 1 0\n" +
+                        "- + - - + - - + - - + - - + - -\n" +
+                        "3 |   3 |   6 |   9 | 1 2 | 1 5\n" +
+                        "- + - - + - - + - - + - - + - -\n" +
+                        "4 |   4 |   8 | 1 2 | 1 6 | 2 0\n" +
+                        "- + - - + - - + - - + - - + - -\n" +
+                        "5 |   5 | 1 0 | 1 5 | 2 0 | 2 5\n" +
+                        "- + - - + - - + - - + - - + - -\n");
     }
 }
